cd69c3ec99bba37347315040c0e48bed2eb20363
fgfgffgfgf <makhovgyu24@st.ithub.ru>
2026-04-20 13:03:46 +0300

    Delete zadanie2.py

--- zadanie2.py
+++
@@ -1,7 +0,0 @@
-def filtered_books(books, term):
-    term_lower = term.lower()
-    result = []
-    for isbn, title, author, qty, price in books:
-        if term_lower in title.lower():
-            result.append([isbn, f"{title}, {author}", qty, price])
-    return result
